0509998379865c83a6b6183038c40283828db89c
dylanglover <dylan.glover@loop.colum.edu>
2016-09-20 21:30:29 -0500

    Delete main.js

--- js-exercises-master/Exercise 2/js/main.js
+++
@@ -1,8 +0,0 @@
-// ---------------------------------------
-// Exersize 2: Complete this daysOfTheWeek array to include all 7 days
-// hint: use splice, push, and unshift
-
-var daysOfTheWeek = ['Monday','Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-// do not touch or write past this line
-$('body').append('<h1>' + daysOfTheWeek.join(', ') + '</h1>');
